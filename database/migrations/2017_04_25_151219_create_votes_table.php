b301d637470aee0ac47f04b50124ace13c7c9ffb
Cayden <cayden.simler@hotmail.com>
2017-04-25 11:21:33 -0500

    Changed the type of vote in the votes table to a boolean instead of an integer.

--- database/migrations/2017_04_25_151219_create_votes_table.php
+++ database/migrations/2017_04_25_151219_create_votes_table.php
@@ -8,7 +8,7 @@ class CreateVotesTable extends Migration {
     public function up() {
         Schema::create('votes', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('vote');
+            $table->boolean('vote');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->integer('post_id')->unsigned();

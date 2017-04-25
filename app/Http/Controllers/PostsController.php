465a3cd95bcc896328e9d023260fa29eeed2b426
Cayden <cayden.simler@hotmail.com>
2017-04-25 15:02:14 -0500

    Add resource routes and test GET routes for posts.

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // show all the posts
+        return "A listing of all posts.";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // show the form to create a post
+        return "Show the form to create a post.";
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // save the new post
+        return "Save the new post.";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // show a specific post based on the id
+        return "Show a specific post with the post id of $id.";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // Show the form for editing the specified resource.
+        return "This will present the current data for the post of id $id and outputs that data onto a form insde the input fields.";
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Update the post in the database
+        return "Update the edited post in the database with the id of $id.";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // delete a post based on the id
+        return "Delete a post with the post id of $id.";
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,59 +16,101 @@ Route::get('/', function () {
 });
 
 
-Route::get('/uppercase/{string}', function ($string) {
-	if ($string == 'cayden') {
-		header("Location: /cayden");
-	} else if (is_string($string) && !is_numeric($string) && $string != "cayden") {
-		return '<h1>' . strtoupper($string) . '</h1>';
-	} else {
-		return "<h1>Input should be a string you doorknob.</h1>";
-	}
-});
+// Route::get('/uppercase/{string}', function ($string) {
+// 	if ($string == 'cayden') {
+// 		header("Location: /cayden");
+// 	} else if (is_string($string) && !is_numeric($string) && $string != "cayden") {
+// 		return '<h1>' . strtoupper($string) . '</h1>';
+// 	} else {
+// 		return "<h1>Input should be a string you doorknob.</h1>";
+// 	}
+// });
 
-Route::get('/cayden', function() {
-	return "<h1>Hello, Cayden!</h1>";
-});
-
-
-Route::get('/increment/{number}', function ($number) {
-	if (is_numeric($number)) {
-		return '<h1>' . $number + 1 . '</h1>';
-	} else {
-		return "<h1>Input should be a number you doorknob.</h1>";
-	}
-});
+// Route::get('/cayden', function() {
+// 	return "<h1>Hello, Cayden!</h1>";
+// });
 
 
-Route::get('/add/{a}/{b}', function($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		return '<h1>' . ($a + $b) . '</h1>';
-	} else if (!is_numeric($a) && is_numeric($b)) {
-		return "<h1>The first input should be a number you doorknob.</h1>";
-	} else if (is_numeric($a) && !is_numeric($b)) {
-		return "<h1>The second input should be a number you doorknob.</h1>";
-	} else {
-		return "<h1>Both inputs should be a number you doorknob.</h1>";
-	}
-});
+// Route::get('/increment/{number}', function ($number) {
+// 	if (is_numeric($number)) {
+// 		return '<h1>' . $number + 1 . '</h1>';
+// 	} else {
+// 		return "<h1>Input should be a number you doorknob.</h1>";
+// 	}
+// });
 
 
-Route::get('/rolldice/{guess?}', function($guess = null) {
-	$randomNumber = mt_rand(1, 6);
-	$data = ['randomNumber' => $randomNumber, 'guess' => $guess];
-	return view('roll-dice', $data);
-});
+// Route::get('/add/{a}/{b}', function($a, $b) {
+// 	if (is_numeric($a) && is_numeric($b)) {
+// 		return '<h1>' . ($a + $b) . '</h1>';
+// 	} else if (!is_numeric($a) && is_numeric($b)) {
+// 		return "<h1>The first input should be a number you doorknob.</h1>";
+// 	} else if (is_numeric($a) && !is_numeric($b)) {
+// 		return "<h1>The second input should be a number you doorknob.</h1>";
+// 	} else {
+// 		return "<h1>Both inputs should be a number you doorknob.</h1>";
+// 	}
+// });
 
 
-Route::get('/sayhello/{name}', function($name)
-{
-    if ($name == "Chris") {
-        return Redirect::to('/');
-    }
+// Route::get('/rolldice/{guess?}', function($guess = null) {
+// 	$randomNumber = mt_rand(1, 6);
+// 	$data = ['randomNumber' => $randomNumber, 'guess' => $guess];
+// 	return view('roll-dice', $data);
+// });
 
-    $data = array('name' => $name);
-    return view('my-first-view', $data);
-});
+
+// Route::get('/sayhello/{name}', function($name)
+// {
+//     if ($name == "Chris") {
+//         return Redirect::to('/');
+//     }
+
+//     $data = array('name' => $name);
+//     return view('my-first-view', $data);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('/uppercase/{word}', 'ExampleController@uppercased');
+// Route::get('/rolldice/{guess}', 'ExampleController@rollDice');
+// Route::get('/increment/{number?}', 'ExampleController@increment');
+// Route::get('/add/{a}/{b}', 'ExampleController@addition');
+
+
+
+
+
+
+
+
+
+
+// CRUD Operations for Posts
+// Route::get('/posts', 'PostsController@index'); // show all the posts
+// Route::get('/posts/create', 'PostsController@create'); // show the form to create a post
+// Route::post('/posts', 'PostsController@store'); // save the new post
+// Route::get('/posts/{post}', 'PostsController@show'); // show a specific post (by id)
+// Route::get('/posts/{post}/edit', 'PostsController@edit'); // show the form to edit a post
+// Route::put('/posts/{post}', 'PostsController@update'); // update the post in the database
+// Route::delete('/posts/{post}', 'PostsController@destroy'); // delete a post (by id)
+
+Route::resource('posts', 'PostsController'); // a resource controller
 
 
 
